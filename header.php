9e55c9f6ee71db94a8cc6a03acf77bda4f269bc4
jyyyann <64628580+jyyyann@users.noreply.github.com>
2022-10-02 22:41:01 +1300

    Added username, navbar depends on session

--- header.php
+++ header.php
@@ -14,25 +14,28 @@ if(mysqli_connect_errno()){
 <head>
     <title> YNOT </title>
     <meta charset=""utf-8">
+    <script src="https://kit.fontawesome.com/af9d7d3ea3.js" crossorigin="anonymous"></script>
     <link rel='stylesheet' type='text/css' href='css/style.css'>
 
 </head>
 
 <body>
 <header>
-    <h1> YNOT </h1>
+    <a class='page' href='index.php'>
+        <h1> YNOT </h1>
+    </a>
     <!--- Navigation Bar -->
     <nav>
-        <a class='page' href='index.php'> Home</a>
         <a class='page' href='stories.php'> Stories</a>
         <?php
             if (isset($_SESSION["username"])) {
-                echo "<a class='page' href='profile.php'> Profile</a>";
-                echo "<a class='page' href='process_logout.php'> Log out</a>";
+                echo "<i class=\"fa-regular fa-user\"></i>";
+                echo "<a class='page' href='profile.php'>".$_SESSION["username"]."</a>";
+                echo "<a class='page' href='process_logout.php'><button class=\"btn-1\"> Log out</button></a>";
             }
             else{
-                echo "<a class='page' href='signup.php'> Sign up</a>";
-                echo "<a class='page' href='login.php'> Log in</a>";
+                echo "<a class='page' href='signup.php'><button class=\"btn-1\"> Sign up </button></a>";
+                echo "<a class='page' href='login.php'><button class=\"btn-2\"> Log in </button></a>";
             }
         ?>
     </nav>
